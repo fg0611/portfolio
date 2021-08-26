0638e523828abf5889b7d9fb6f16b8bf11d5f719
francisco garrido <fg0611@gmail.com>
2021-08-26 20:02:18 -0300

    added icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,28 @@
 import React from "react";
 import "./styles.css";
-export default (props) => <div className="footer">Footer</div>;
+
+export default (props) => (
+  <div className="footer">
+    <button
+      onClick={() => {
+        window.open("https://github.com/fg0611");
+      }}
+    >
+      <i class="fa fa-github" aria-hidden="true"></i>
+    </button>
+    <button
+      onClick={() => {
+        window.open("https://www.linkedin.com/in/frangarrido0611/");
+      }}
+    >
+      <i class="fa fa-linkedin" aria-hidden="true"></i>
+    </button>
+    <button
+      onClick={() => {
+        window.open("https://www.linkedin.com/in/frangarrido0611/");
+      }}
+    >
+      <i class="fa fa-envelope" aria-hidden="true"></i>
+    </button>
+  </div>
+);
